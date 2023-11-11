cca84a3e9548da217d92bc690a12757195270754
Your Name <deckerwill7@gmail.com>
2023-11-11 15:44:04 -0600

    fix `spr()` error and create `sprf()`

--- R/spr.R
+++ R/spr.R
@@ -1,16 +1,35 @@
 #' Calculate semi-partial correlations from data
 #'
-#' @param f Formula for full model
-#' @param vint Independent variable of interest
-#' @param data Data frame correlation matrix
+#' @param dep Dependent variable
+#' @param ivi Independent variable of interest
+#' @param ivn Independent variable of NO interest
 #' @return Semi-partial correlation value
 #'
 #' @export
 
-spr <- function(f, vint, data ){
-  x <- cor(f[[2]],
-      resid(lm(labels(terms(f))[1] ~ labels(terms(f))[1]),
-            data = data))
+spr <- function(dep, ivi, ivn){
 
-  return(x)
+  x <- cor(dep, resid(lm(ivi ~ ivn)))
+
+  return(c("Semi-partial correlation" = x))
+}
+
+#' Calculate semi-partial correlations from data by specifying a formula.
+#'
+#' @param f Formula for entire model in which label[1] is your independent variable of interest.
+#' @param data Data.frame of data
+#' @return Semi-partial correlation value
+#'
+#' @export
+
+sprf <- function(f, data){
+
+  y <- data[as.character(formula.tools::lhs(f))]
+  indep <- labels(terms(f))
+  ivi <- as.matrix(data[indep[1]])
+  ivn <- as.matrix(data[indep[2]])
+
+  x <- (cor(y, ivi) - (cor(ivi, ivn) * cor(ivn, y)))/sqrt(1-cor(ivi, ivn)^2)
+
+  return(c("Semi-partial correlation" = x))
 }
